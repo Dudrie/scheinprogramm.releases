ab6eedd9dc87bd806b31ff8d530a94fbfc7475c1
Dudrie <Dudrie@gmx.de>
2018-07-02 17:11:56 +0200

    JSDoc

--- src/helpers/FontAwesomeInit.tsx
+++ src/helpers/FontAwesomeInit.tsx
@@ -3,6 +3,9 @@ import { fal } from '@fortawesome/pro-light-svg-icons';
 import { far } from '@fortawesome/pro-regular-svg-icons';
 import { fas } from '@fortawesome/pro-solid-svg-icons';
 
+/**
+ * Initialzies the Font Awesome icon library.
+ */
 export function initFontAwesome() {
     library.add(fal, far, fas);
     console.info('[INFO] initFontAwesome -- FontAwesome icons added to librabry.');
