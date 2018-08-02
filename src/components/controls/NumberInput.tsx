71dc197bcd6ce731d693b370478e76fdb83bffc0
Dudrie <Dudrie@gmx.de>
2018-08-02 13:54:37 +0200

    Fix: modifiedStepSize wird nicht weitergegeben.

--- src/components/controls/NumberInput.tsx
+++ src/components/controls/NumberInput.tsx
@@ -82,7 +82,7 @@ class NumberInputClass extends React.Component<PropType, State> {
     }
 
     render() {
-        let { value, minValue, maxValue, showButtons, onValueChanged, disabled, defaultValue, classes, helperText, InputProps, ...other } = this.props;
+        let { value, minValue, maxValue, showButtons, onValueChanged, disabled, defaultValue, classes, helperText, modifiedStepSize, InputProps, ...other } = this.props;
         let btnWidth: number = showButtons ? this.INPUT_HEIGHT : 0;
 
         let disablePlus: boolean = this.state.value >= this.maxValue;
