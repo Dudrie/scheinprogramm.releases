b0fb1afb62e9cea9aec98e99ee72b3065a350b31
Dudrie <Dudrie@gmx.de>
2018-09-07 22:24:06 +0200

    ADD - Korrekte Schein-Information in der LectureOverview

--- src/renderer/view/LectureOverview.tsx
+++ src/renderer/view/LectureOverview.tsx
@@ -1,28 +1,33 @@
-import { Grid, Slide, StyleRulesCallback, Theme, Typography, withStyles, WithStyles, List, ListItem } from '@material-ui/core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Grid, List, ListItem, Slide, StyleRulesCallback, Theme, Tooltip, Typography, withStyles, WithStyles } from '@material-ui/core';
+import { green, orange, red } from '@material-ui/core/colors';
 import * as React from 'react';
+import { CONTENT_PADDING } from '../App';
 import { CreateBar } from '../components/bars/CreateBar';
 import { SheetBar } from '../components/bars/SheetBar';
 import { SheetEditor } from '../components/editors/SheetEditor';
-import Language from '../helpers/Language';
 import { SystemOverviewBox, SystemOverviewBoxIcon } from '../components/SystemOverviewBox';
-import { DataService } from '../helpers/DataService';
 import { LectureSystem, SystemType } from '../data/LectureSystem';
-import { Sheet, Points } from '../data/Sheet';
+import { Points, Sheet } from '../data/Sheet';
+import { DataService } from '../helpers/DataService';
+import Language from '../helpers/Language';
 import { NotificationService } from '../helpers/NotificationService';
-import { CONTENT_PADDING } from '../App';
 
-interface State {
-    isCreatingSheet: boolean;
-    sheetToEdit: Sheet | undefined;
+enum AchieveState {
+    ACHIEVED, CAN_BE_ACHIEVED, PROBABLY_ACHIEVED, ALMOST_ACHIEVED, NOT_ACHIEVABLE, NO_INFO_AVAILABLE
 }
 
 type LectureOverviewClassKey =
     | 'root'
     | 'sheetBox'
     | 'statBox'
-    | 'statGeneralInfo'
     | 'statTitle'
-    | 'listItemDenseOverride';
+    | 'statGeneralInfo'
+    | 'statGeneralInfoIcon'
+    | 'listItemDenseOverride'
+    | 'achievedColor'
+    | 'almostAchievedColor'
+    | 'notAchievedColor';
 
 const style: StyleRulesCallback<LectureOverviewClassKey> = (theme: Theme) => ({
     root: {
@@ -35,7 +40,7 @@ const style: StyleRulesCallback<LectureOverviewClassKey> = (theme: Theme) => ({
         overflowX: 'hidden'
     },
     statBox: {
-        minWidth: '225px',
+        minWidth: '250px',
         marginBottom: (theme.spacing.unit / 1) + 'px',
         paddingLeft: theme.spacing.unit * 2 + 'px',
         height: '100%',
@@ -51,6 +56,9 @@ const style: StyleRulesCallback<LectureOverviewClassKey> = (theme: Theme) => ({
     statGeneralInfo: {
         padding: theme.spacing.unit / 2
     },
+    statGeneralInfoIcon: {
+        marginRight: theme.spacing.unit / 2
+    },
     statTitle: {
         color: theme.palette.primary.light,
         marginBottom: 0
@@ -61,10 +69,24 @@ const style: StyleRulesCallback<LectureOverviewClassKey> = (theme: Theme) => ({
         '&:first-of-type': {
             paddingTop: 0
         }
+    },
+    achievedColor: {
+        color: green['400']
+    },
+    almostAchievedColor: {
+        color: orange['400']
+    },
+    notAchievedColor: {
+        color: red['400']
     }
 });
 
 type PropType = object & WithStyles<LectureOverviewClassKey>;
+
+interface State {
+    isCreatingSheet: boolean;
+    sheetToEdit: Sheet | undefined;
+}
 
 class LectureOverviewClass extends React.Component<PropType, State> {
     constructor(props: PropType) {
@@ -83,6 +105,8 @@ class LectureOverviewClass extends React.Component<PropType, State> {
         let totalSheetCount = DataService.getActiveLectureTotalSheetCount();
         let currentSheetCount = DataService.getActiveLectureCurrentSheetCount();
         let showCreateBar: boolean = (totalSheetCount == 0) || (currentSheetCount < totalSheetCount);
+
+        let generalInformation = this.generateGeneralInformation();
 
         return (
             <Grid
@@ -144,10 +168,13 @@ class LectureOverviewClass extends React.Component<PropType, State> {
                     </Typography>
 
                     <div className={this.props.classes.statGeneralInfo} >
-                        <Typography variant='body2' >
-                            {/* TODO: Richtigen Text anzeigen! */}
-                            Schein kann (nicht) erreicht werden.
-                        </Typography>
+                        <Tooltip
+                            title={generalInformation.addTooltip}
+                        >
+                            <Typography variant='body1' className={generalInformation.colorClass} >
+                                {generalInformation.text}
+                            </Typography>
+                        </Tooltip>
                         {DataService.hasActiveLecturePresentation() && (
                             <Typography variant='body1' >
                                 Vorrechenpunkte: {presPoints.achieved + ' / ' + presPoints.total}
@@ -159,6 +186,141 @@ class LectureOverviewClass extends React.Component<PropType, State> {
                 </Grid>
             </Grid >
         );
+    }
+
+    private checkIfAchievable(): AchieveState {
+        // Check, if there's a total sheet count in the active lecture. If not, we cannot estimate things.
+        let activeLecture = DataService.getActiveLecture();
+        if (activeLecture && activeLecture.totalSheetCount == 0) {
+            return AchieveState.NO_INFO_AVAILABLE;
+        }
+
+        let achieveState: AchieveState = AchieveState.CAN_BE_ACHIEVED;
+        let lectureSystems: LectureSystem[] = DataService.getActiveLectureSystems();
+        let sheetsRemaining = DataService.getActiveLectureTotalSheetCount() - DataService.getActiveLectureCurrentSheetCount();
+
+        // Check all the system if they are achieved.
+        for (let i = 0; i < lectureSystems.length; i++) {
+            const lecSys = lectureSystems[i];
+
+            let points = DataService.getActiveLecturePointsOfSystem(lecSys.id);
+            let pointsPerFutureSheet = this.calculatePointsPerFutureSheets(lecSys, points.achieved, points.total);
+
+            // If there's one system not achievable consider it NOT_ACHIEVABLE in general
+            if (pointsPerFutureSheet == Number.NEGATIVE_INFINITY) {
+                achieveState = AchieveState.NOT_ACHIEVABLE;
+                break;
+            }
+
+            // That system is considered achieved if it's the first consider everything achieved (achieved systems later won't change that).
+            // However, if there's a system not achieved yet, 'reset' the state - it'll not change back.
+            if (pointsPerFutureSheet == 0) {
+                if (i == 0) {
+                    achieveState = AchieveState.ACHIEVED;
+                }
+
+                if (achieveState == AchieveState.ACHIEVED && lecSys.pointsPerSheet == 0 && sheetsRemaining > 0) {
+                    // The LectureSystem uses an estimation and there are sheets remaining, so we're careful with predictions.
+                    achieveState = AchieveState.PROBABLY_ACHIEVED;
+                }
+            } else {
+                achieveState = AchieveState.CAN_BE_ACHIEVED;
+            }
+        }
+
+        // If we're not achievable at this point we won't be further down the process.
+        if (achieveState == AchieveState.NOT_ACHIEVABLE) {
+            return achieveState;
+        }
+
+        // Check if the presentation points are met
+        if (achieveState == AchieveState.ACHIEVED) {
+            let presentationPoints = DataService.getActiveLecturePresentationPoints();
+
+            if (presentationPoints.achieved < presentationPoints.total) {
+                achieveState = AchieveState.ALMOST_ACHIEVED;
+            }
+        }
+
+        return achieveState;
+    }
+
+    private generateGeneralInformation(): { text: React.ReactNode, colorClass: string, addTooltip: string } {
+        let text: React.ReactNode = '';
+        let colorClass: string = '';
+        let addTooltip: string = '';
+
+        let achieveState = this.checkIfAchievable();
+
+        switch (achieveState) {
+            case AchieveState.CAN_BE_ACHIEVED:
+                text = Language.getString('LECTURE_OVERVIEW_INFO_CAN_BE_ACHIEVED');
+                break;
+
+            case AchieveState.PROBABLY_ACHIEVED:
+                text = (<>
+                    <FontAwesomeIcon
+                        icon={{ prefix: 'fas', iconName: 'question' }}
+                        className={this.props.classes.statGeneralInfoIcon}
+                    />
+                    {Language.getString('LECTURE_OVERVIEW_INFO_PROBABLY_ACHIEVED')}
+                </>);
+                addTooltip = Language.getString('LECTURE_OVERVIEW_INFO_PROBABLY_ACHIEVED_TOOLTIP');
+                colorClass = this.props.classes.almostAchievedColor;
+                break;
+
+            case AchieveState.ACHIEVED:
+                text = (<>
+                    <FontAwesomeIcon
+                        icon={{ prefix: 'fas', iconName: 'check' }}
+                        className={this.props.classes.statGeneralInfoIcon}
+                    />
+                    {Language.getString('LECTURE_OVERVIEW_INFO_ACHIEVED')}
+                </>);
+                addTooltip = Language.getString('LECTURE_OVERVIEW_INFO_ACHIEVED_TOOLTIP');
+                colorClass = this.props.classes.achievedColor;
+                break;
+
+            case AchieveState.NOT_ACHIEVABLE:
+                text = (<>
+                    <FontAwesomeIcon
+                        icon={{ prefix: 'fas', iconName: 'times' }}
+                        className={this.props.classes.statGeneralInfoIcon}
+                    />
+                    {Language.getString('LECTURE_OVERVIEW_INFO_NOT_ACHIEVABLE')}
+                </>);
+                addTooltip = Language.getString('LECTURE_OVERVIEW_INFO_NOT_ACHIEVABLE_TOOLTIP');
+                colorClass = this.props.classes.notAchievedColor;
+                break;
+
+            case AchieveState.ALMOST_ACHIEVED:
+                text = (<>
+                    <FontAwesomeIcon
+                        icon={{ prefix: 'fas', iconName: 'info' }}
+                        className={this.props.classes.statGeneralInfoIcon}
+                    />
+                    {Language.getString('LECTURE_OVERVIEW_INFO_ALMOST_ACHIEVED')}
+                </>);
+                addTooltip = Language.getString('LECTURE_OVERVIEW_INFO_ALMOST_ACHIEVED_TOOLTIP');
+                colorClass = this.props.classes.almostAchievedColor;
+                break;
+
+            case AchieveState.NO_INFO_AVAILABLE:
+                text = (<>
+                    <FontAwesomeIcon
+                        icon={{ prefix: 'fas', iconName: 'info' }}
+                        className={this.props.classes.statGeneralInfoIcon}
+                    />
+                    {Language.getString('LECTURE_OVERVIEW_INFO_NO_INFO_AVAILABLE')}
+                </>);
+                addTooltip = Language.getString('LECTURE_OVERVIEW_INFO_NO_INFO_AVAILABLE_TOOLTIP');
+                break;
+
+            default:
+                console.error(`[ERROR] LectureOverview::getInformationText -- No case for the AchieveState ${achieveState} given.`);
+        }
+
+        return { text, colorClass, addTooltip };
     }
 
     private generateSystemOverviewBox(system: LectureSystem): JSX.Element {
