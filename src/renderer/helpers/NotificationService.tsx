02767db07e288d8201f83e5217a64ac30f84d198
Dudrie <Dudrie@gmx.de>
2018-10-01 18:56:06 +0200

    ADD - NotificationService exportiert den Type "Notification"

--- src/renderer/helpers/NotificationService.tsx
+++ src/renderer/helpers/NotificationService.tsx
@@ -5,6 +5,7 @@ import * as React from 'react';
 import * as NotificationSystem from 'react-notification-system';
 import { Notification, System } from 'react-notification-system';
 
+export type Notification = Notification;
 export type NotificationEventAddInfo = {
     action?: {
         label: string;
