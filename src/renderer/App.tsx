8bc95c6d599b8bfb98b90504bcabc505b278b717
Dudrie <Dudrie@gmx.de>
2018-10-11 23:06:05 +0200

    ADD - AppIcon

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -122,7 +122,6 @@ const keyMap: KeyMap = {
     'ctrlN': 'ctrl+n'
 };
 
-// TODO: AppIcon hinzufügen!
 class AppClass extends React.Component<WithStyles<typeof style>, State> {
     constructor(props: WithStyles<typeof style>) {
         super(props);
