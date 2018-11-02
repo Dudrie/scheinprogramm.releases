177051fcfdfc99e27c5a18a2bb74abe63c8b835c
Dudrie <Dudrie@gmx.de>
2018-11-02 15:52:32 +0100

    REMOVE - webpage Ordner

--- webpage/src/WebApp.test.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import { WebApp } from './WebApp';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<WebApp />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
